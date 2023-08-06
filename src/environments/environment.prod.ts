4bd3408e227b6dcb09e73c969fc8877048e54e34
ramkumarm15 <ramkumarmani2000@gmail.com>
2023-08-06 21:16:46 +0530

    production api url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  AUTH_API: 'https://online-shopping-asp-production.azurewebsites.net/api/Authentication',
+  USER_API: 'https://online-shopping-asp-production.azurewebsites.net/api/Users',
+  PRODUCT_API: 'https://online-shopping-asp-production.azurewebsites.net/api/Product',
+  BILLING_ADDRESS_API: 'https://online-shopping-asp-production.azurewebsites.net/api/BillingAddress',
+  CART_API: {
+    CART: 'https://online-shopping-asp-production.azurewebsites.net/api/Cart',
+    CART_ITEM: 'https://online-shopping-asp-production.azurewebsites.net/api/CartItem',
+  }
 };
